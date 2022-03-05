b0431a155f51f93f69d4525902e147df05791da3
Aitor Corchero Rodríguez <aitorcorchero@gmail.com>
2022-03-05 21:05:07 +0100

    Codification of AQUASPICE CS Agricola

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ const parser = new N3.Parser();
 
 const rdfStream = fs.createReadStream('./wisis.ttl');
 const rdfStreamExample = fs.createReadStream('./examples/ULTIMATE-CS2.ttl');
+const rdfStreamExampleAgricola = fs.createReadStream('./examples/AQUASPICE-AGRICOLA.ttl');
 
 parser.parse(rdfStream, console.log);
